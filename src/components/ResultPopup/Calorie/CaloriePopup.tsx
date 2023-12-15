ffb52ef79a1c6ebe583bc93e17dfee9bc13e263e
ursgre <86769021+ursgre@users.noreply.github.com>
2023-12-15 13:14:59 +0100

    fix date-picker problem

--- src/components/ResultPopup/Calorie/CaloriePopup.tsx
+++ src/components/ResultPopup/Calorie/CaloriePopup.tsx
@@ -1,58 +1,59 @@
-import React from 'react'
-import '../popup.css'
+import React from 'react';
+import '../popup.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import DatePicker from "react-horizontal-datepicker";
-import { AiFillDelete, AiOutlineClose } from 'react-icons/ai'
+import { AiFillDelete, AiOutlineClose } from 'react-icons/ai';
 import { MultiSectionDigitalClock } from '@mui/x-date-pickers/MultiSectionDigitalClock';
 import dayjs, { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 interface CaloriePopupProps {
   setShowCaloriePopup: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const CaloriePopup: React.FC<CaloriePopupProps> = ({ setShowCaloriePopup }) => {
-  const color = '#03c9bf'
+  const color = '#03c9bf';
 
-  const [date, setDate] = React.useState<any>(new Date())
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(new Date());
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
 
-  const selectedDay = (val: any) => {
-    console.log(val)
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
   };
 
-
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
   return (
     <div className='popupout'>
-
-
       <div className='popupbox'>
-        <button className='close'
+        <button
+          className='close'
           onClick={() => {
-            setShowCaloriePopup(false)
+            setShowCaloriePopup(false);
           }}
         >
           <AiOutlineClose />
         </button>
 
-        <DatePicker getSelectedDay={selectedDay}
-          endDate={100}
-          selectDate={new Date()}
-          labelFormat={"MMMM"}
-          color={color}
+        <TextField
+          id="date-picker"
+          label="Choose Date"
+          type="date"
+          defaultValue={dayjs(selectedDate || undefined).format('YYYY-MM-DD')}
+          onChange={(e) => handleDateChange(new Date(e.target.value))}
+          InputLabelProps={{
+            shrink: true,
+          }}
         />
 
         <TextField id="outlined-basic" label="Food item name" variant="outlined" color="warning" />
         <TextField id="outlined-basic" label="Food item amount (in gms)" variant="outlined" color="warning" />
         <div className='timebox'>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <MultiSectionDigitalClock
+            <MultiSectionDigitalClock
               value={value}
               onChange={(newValue) => setValue(newValue)}
             />
           </LocalizationProvider>
-
         </div>
         <Button variant="contained" >
           Save
@@ -68,19 +69,16 @@ const CaloriePopup: React.FC<CaloriePopupProps> = ({ setShowCaloriePopup }) => {
             <h3>Banana</h3>
             <h3>200 gms</h3>
             <button> <AiFillDelete /></button>
-
           </div>
           <div className='item'>
             <h3>Rice</h3>
             <h3>300 gms</h3>
             <button> <AiFillDelete /></button>
-
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default CaloriePopup
+export default CaloriePopup;
